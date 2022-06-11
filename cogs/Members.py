ee63a67c01d005728514dfd9fb7b62b0ca35bfb7
StefanKulik <stefan.kulik@t-online.de>
2022-06-11 22:58:48 +0200

    V2.2

--- cogs/Members.py
+++ cogs/Members.py
@@ -1,20 +1,20 @@
 from discord import Member, Embed
 from discord.ext import commands
 
+from config.util import get_prefix, send_embed
+
 
 class Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="userinfo", aliases=["ui"],
-                      description="Zeigt Info über ein Mitglied",
-                      help="userinfo <Member>")
-    @commands.cooldown(1, 30, commands.BucketType.user)
+    @commands.slash_command(name="userinfo",
+                            description="Zeigt Info über ein Mitglied")
     async def userinfo(self, ctx, member: Member = None):
         if member:
             embed = Embed(title=f"Userinfo für {member.name}",
-                                  description=f"Dies ist eine Userinfo für den User {member.mention}",
-                                  color=0x22a7f0)
+                          description=f"Dies ist eine Userinfo für den User {member.mention}",
+                          color=0x22a7f0)
             embed.add_field(name="Server beigetreten", value=member.joined_at.strftime("%d/%m/%Y, %H:%M:%S"),
                             inline=True)
             embed.add_field(name="Discord beigetreten", value=member.created_at.strftime("%d/%m/%Y, %H:%M:%S"),
@@ -25,12 +25,12 @@ class Members(commands.Cog):
                     rollen += f"{role.mention} \r\n"
             if rollen:
                 embed.add_field(name="Rollen", value=rollen, inline=False)
-            embed.set_thumbnail(url=member.avatar_url)
-            embed.set_footer(text=f"*{await util.get_prefix(self.bot, ctx.message)}help* for more info")
+            embed.set_thumbnail(url=member.avatar)
+            embed.set_footer(text=f"*LEL* for more info")
         else:
-            embed = discord.Embed(title=f"Userinfo für {ctx.author.name}",
-                                  description=f"Dies ist eine Userinfo für den User {ctx.author.mention}",
-                                  color=0x22a7f0)
+            embed = Embed(title=f"Userinfo für {ctx.author.name}",
+                          description=f"Dies ist eine Userinfo für den User {ctx.author.mention}",
+                          color=0x22a7f0)
             embed.add_field(name="Server beigetreten", value=ctx.author.joined_at.strftime("%d/%m/%Y, %H:%M:%S"),
                             inline=True)
             embed.add_field(name="Discord beigetreten", value=ctx.author.created_at.strftime("%d/%m/%Y, %H:%M:%S"),
@@ -41,7 +41,12 @@ class Members(commands.Cog):
                     rollen += f"{role.mention} \r\n"
             if rollen:
                 embed.add_field(name="Rollen", value=rollen, inline=False)
-            embed.set_thumbnail(url=ctx.author.avatar_url)
-            embed.set_footer(text=f"*{await util.get_prefix(self.bot, ctx.message)}help* for more info")
+            embed.set_thumbnail(url=ctx.author.avatar)
+            embed.set_footer(text=f"*LEL* for more info")
 
-        await util.send_embed(ctx, embed, 15)
+        await send_embed(ctx, embed, 15)
+        await ctx.respond('T')
+
+
+def setup(bot):
+    bot.add_cog(Members(bot))
